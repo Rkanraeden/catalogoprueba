db411206e55f24a7e06c9670a5da2800cf6064d2
Rkanraeden <lecortezc@gmail.com>
2022-11-17 03:56:51 -0300

    feat devise gem and pg gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
 
-  # Defines the root path route ("/")
-  # root "wiews#index"
 end
